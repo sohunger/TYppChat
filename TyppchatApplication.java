07e319de70473f4d40290c5c30cc267333d64d79
sohunger <1536478643@qq.com>
2022-01-22 21:17:48 +0800

    腾云pp聊天室

---
+++ TyppchatApplication.java
@@ -0,0 +1,13 @@
+package com.huang.typpchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyppchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyppchatApplication.class, args);
+    }
+
+}
