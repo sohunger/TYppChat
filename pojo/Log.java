4d811b9db1f7832386179227beab98c8270450f3
sohunger <84076257+sohunger@users.noreply.github.com>
2022-07-20 21:32:06 +0800

    log实体类添加添加a属性

--- pojo/Log.java
+++ pojo/Log.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +17,5 @@ public class Log {
     private String logDetail; //详情
     private String logIp; //ip
     private String hh;
+    private Date a;
 }
